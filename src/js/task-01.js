0b0536683061c5ab9cba66937f059c9cde35901a
Roman Usenko <luckyjuse@gmail.com>
2023-02-11 17:15:32 +0200

    delete dublicate

--- src/js/task-01.js
+++
@@ -1,7 +0,0 @@
-const categoryItemsEl = document.querySelectorAll('.item');
-console.log('Number of categories:', categoryItemsEl.length);
-
-categoryItemsEl.forEach(element => {
-  console.log('Category:', element.firstElementChild.textContent);
-  console.log('Elements', element.querySelectorAll('li').length);
-});
